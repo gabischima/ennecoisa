19bdcf1978d41cb4fc286b8217ac81df955efebc
Gabriela Mauricio <gabriela.schirmer@mundipagg.com>
2020-01-13 00:28:13 -0300

    ✨add itemcollectionviewcell

---
+++ ennecoisa/ItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCollectionViewCell.swift
+//  ennecoisa
+//
+//  Created by Gabriela Schirmer Mauricio on 11/01/20.
+//  Copyright © 2020 Gabriela Schirmer | MundiPagg. All rights reserved.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+}
