006fb50ac0796c4d6a06d119402f6edac5a8e5d9
Gabriela Mauricio <gabriela.schirmer@mundipagg.com>
2020-05-30 00:59:22 -0300

    ✨add constants and ennesection files

---
+++ ennecoisa/Model/EnneSection.swift
@@ -0,0 +1,39 @@
+//
+//  EnneSection.swift
+//  ennecoisa
+//
+//  Created by Gabriela Schirmer Mauricio on 29/05/20.
+//  Copyright © 2020 Gabriela Schirmer. All rights reserved.
+//
+
+import UIKit
+
+struct EnneSection: Equatable {
+    let slug: String
+    var selected: Bool? = false
+    var icon: UIImage? {
+        return UIImage(named: slug)
+    }
+    let size: Int
+    var images: [EnneImage] {
+        var arr: [EnneImage] = []
+        for item in 0..<(size) {
+            arr.append(EnneImage(slug: "\(slug)_\(item)"))
+        }
+        return arr
+    }
+    
+    static func == (lhs: EnneSection, rhs: EnneSection) -> Bool {
+        return lhs.slug == rhs.slug
+    }
+}
+
+struct EnneImage {
+    let slug: String
+    var icon: UIImage? {
+        return UIImage(named: "\(slug)_icon")
+    }
+    var image: UIImage? {
+       return UIImage(named: "\(slug)")
+   }
+}
---
+++ ennecoisa/Utils/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  ennecoisa
+//
+//  Created by Gabriela Schirmer Mauricio on 22/05/20.
+//  Copyright © 2020 Gabriela Schirmer. All rights reserved.
+//
+
+import UIKit
+
+struct ARSize {
+    static var device: CGSize {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad:
+            return CGSize(width: 2388, height: 1482)
+        case .phone:
+            return CGSize(width: 750, height: 1196)
+        default:
+            return CGSize(width: 750, height: 1196)
+        }
+    }
+}
