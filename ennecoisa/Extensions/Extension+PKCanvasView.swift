e74af36dfce84c42a3fe467edb9f6c912ae73ad1
Gabriela Mauricio <gabismauricio@gmail.com>
2020-11-05 20:17:26 -0300

    ✨ update extensions

---
+++ ennecoisa/Extensions/Extension+PKCanvasView.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+PKCanvasView.swift
+//  ennecoisa
+//
+//  Created by Gabriela Schirmer Mauricio on 22/06/20.
+//  Copyright © 2020 Gabriela Schirmer. All rights reserved.
+//
+
+import PencilKit
+
+extension PKCanvasView {
+    func clearCanvas() {
+        self.drawing = PKDrawing()
+    }
+}
